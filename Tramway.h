7cea076689c3640b8a506630c8762a8b707a11f6
thomaaaas <thomasbfjfn@gmail.com>
2020-01-29 14:04:31 +0100

    Création de la classe Tramway

---
+++ Tramway.h
@@ -0,0 +1,13 @@
+//
+// Created by norma on 29/01/2020.
+//
+
+#ifndef TRAMWAY_TRAMWAY_H
+#define TRAMWAY_TRAMWAY_H
+class Tramway{
+public:
+    Tramway();
+private:
+    double d_vitesseMax, d_vitesseActuelle, d_distanceMin;
+};
+#endif //TRAMWAY_TRAMWAY_H
