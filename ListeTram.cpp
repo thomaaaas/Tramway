0feda3bf85a45457c52f4aa90e8a325e90a4cf47
thomaaaas <thomasbfjfn@gmail.com>
2020-03-19 19:06:38 +0100

    Correction ligne un seul tramway Minimum du projet effectué

--- ListeTram.cpp
+++ ListeTram.cpp
@@ -12,8 +12,7 @@ ListeTram::ListeTram() : t{nullptr}
 
 ListeTram::~ListeTram()
 {
-    while(t)
-	{
+    while(t){
 		ChainonTram *tmp=t->suiv;
 		delete t;
 		t=tmp;
@@ -39,16 +38,13 @@ void ListeTram::effacer(){
 void ListeTram::Trajectoire (ChainonArret *Arret, ChainonTram *tram, vector<double> &Coord)
 {
     double dt, d, a, b, x, y;
-    if(!Arret->getSuiv())
-    {
+    if(!Arret->getSuiv()){
         tram->getTramway()->setSens(false);
     }
-    if(!Arret->getPrec())
-    {
+    if(!Arret->getPrec()){
         tram->getTramway()->setSens(true);
     }
-    if(tram->getTramway()->getSens())
-    {
+    if(tram->getTramway()->getSens()){
         //aller
         dt = sqrt(pow(tram->getTramway()->getX() - Arret->getArret()->getX(),2) + pow(tram->getTramway()->getY()-Arret->getArret()->getY(),2));
         d = sqrt(pow(Arret->getSuiv()->getArret()->getX()-Arret->getArret()->getX(),2) + pow(Arret->getSuiv()->getArret()->getY()-Arret->getArret()->getY(),2));
@@ -58,8 +54,7 @@ void ListeTram::Trajectoire (ChainonArret *Arret, ChainonTram *tram, vector<doub
         x = a*Arret->getSuiv()->getArret()->getX() + b*Arret->getArret()->getX();
         y = a*Arret->getSuiv()->getArret()->getY() + b*Arret->getArret()->getY();
     }
-    else
-    {
+    else{
         //retour
         dt = sqrt(pow(tram->getTramway()->getX() - Arret->getArret()->getX(),2) + pow(tram->getTramway()->getY()-Arret->getArret()->getY(),2));
         d = sqrt(pow(Arret->getPrec()->getArret()->getX()-Arret->getArret()->getX(),2) + pow(Arret->getPrec()->getArret()->getY()-Arret->getArret()->getY(),2));
@@ -69,8 +64,6 @@ void ListeTram::Trajectoire (ChainonArret *Arret, ChainonTram *tram, vector<doub
         x = a*Arret->getPrec()->getArret()->getX() + b*Arret->getArret()->getX();
         y = a*Arret->getPrec()->getArret()->getY() + b*Arret->getArret()->getY();
     }
-
-
     Coord.push_back(x);
     Coord.push_back(y);
 }
@@ -78,91 +71,59 @@ void ListeTram::Trajectoire (ChainonArret *Arret, ChainonTram *tram, vector<doub
 bool ListeTram::distance(ChainonTram *tram)
 {
     ChainonTram *c;
-        if(tram->suiv == nullptr)
-        {
-            c = t;
+    if(tram == t && tram->suiv == nullptr){return true;}
+    if(tram->suiv == nullptr){c = t;}
+    else{c = tram->suiv;}
+    double d = sqrt(pow(tram->getTramway()->getX() - c->getTramway()->getX(),2) + pow(tram->getTramway()->getY()-c->getTramway()->getY(),2));
+    if(tram->getTramway()->getSens() == c->getTramway()->getSens()){
+        if (d < tram->getTramway()->getDistanceMin()){
+            return false;
         }
-        else
-        {
-            c = tram->suiv;
-        }
-        double d = sqrt(pow(tram->getTramway()->getX() - c->getTramway()->getX(),2) + pow(tram->getTramway()->getY()-c->getTramway()->getY(),2));
-        if(tram->getTramway()->getSens() == c->getTramway()->getSens())
-        {
-            if (d < tram->getTramway()->getDistanceMin())
-            {
-                return false;
-            }
-        }
+    }
     return true;
 }
 
 void ListeTram::avancer(Ligne *ligne){
     ChainonTram *c = t;
-    double x,y;
-    double vitesse;
     vector<double> coord;
     while(c){
-        if (c->getTramway()->getonMarche())
-        {
-            x = c->getTramway()->getX();
-            y = c->getTramway()->getY();
-            if(distance(c))
-            {
-                if(ligne->arretTram(c))
-            {
-                c->actuelArret = ligne->arretTram(c);
-                c->getTramway()->setonMarche(false);
-                c->getTramway()->setTempsArret(10);
-            }
+        if (c->getTramway()->getonMarche()){
+            if(distance(c)){
+                if(ligne->arretTram(c)){
+                    c->actuelArret = ligne->arretTram(c);
+                    c->getTramway()->setonMarche(false);
+                    c->getTramway()->setTempsArret(10);
+                }
             Trajectoire(c->actuelArret, c,coord);
             c->getTramway()->setPosition(coord[0], coord[1]);
             coord.clear();
             }
         }
-        else
-        {
-            if(c->getTramway()->getTempsArret() > 0)
-            {
+        else{
+            if(c->getTramway()->getTempsArret() > 0){
                 double tmps = c->getTramway()->getTempsArret();
                 c->getTramway()->setTempsArret(--tmps);
             }
-            else
-            {
+            else{
                 c->getTramway()->setonMarche(true);
             }
         }
-
         c = c->suiv;
     }
-
 }
 
-
-
-/*
-vérifier distance tram suivant stop si trop infd
-si coord = alors stop quelque s
-sinon avance de vitesse avec ligne tracer entre station (equation)
-if sens = true -> station = suiv (+ vitesse)
-if sens = false -> station = prec (-vitesse)
-*/
 void ListeTram::insererTramway(Tramway *tramway)
 {
 	ChainonTram *nc=new ChainonTram(tramway);
-	if(t==nullptr)
-    {
-        t=nc;
-    }
-    else
-    {
-        ChainonTram *precC=nullptr, *c=t;
-        while(c!=nullptr)
-        {
-            precC=c;
-            c=c->suiv;
-        }
-        precC->suiv=nc;
-        nc->suiv = c;
-    }
+	if (t==nullptr){
+		t=nc;
+		return;
+	}
+	ChainonTram *precC=nullptr,*c=t;
+	while(c){
+		precC=c;
+		c=c->suiv;
+	}
+	precC->suiv=nc;
+	nc->suiv=c;
 }
